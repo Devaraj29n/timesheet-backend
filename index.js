ba8ecbfcf406338f9f1e50ccd9b1adf0b2ad83ad
Devaraj Nagaraj <devaraj.nagaraj@tvs.in>
2025-05-23 10:20:41 +0530

    Url Issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 3030;
 // CORS Middleware
 // ----------------------------------------------
 const corsOptions = {
-  origin: 'https://new-timesheet.netlify.app/',
+  origin: 'https://new-timesheet.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
